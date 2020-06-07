71cb67db023ed89c8686b9944779b7b90adb47eb
Kristina Naylor <kristinamarienaylor@gmail.com>
2020-06-06 23:01:47 -0400

    Remove JS link in head and add var pizza and function orderPizza

---
+++ main.js
@@ -0,0 +1,21 @@
+var pizza = {
+    crust: 'thin',
+    cheese: 'mozzerella',
+    sauce: 'tomato',
+    addToppings: (function() {
+        console.log('Please add pepperoni and sausage');
+    })
+};
+
+function orderPizza() {
+    console.log(pizza.crust);
+    console.log(pizza.cheese);
+    console.log(pizza.sauce);
+    console.log(pizza.addToppings());
+}
+
+orderPizza();
+
+
+
+
